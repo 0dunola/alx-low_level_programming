0feb4b7d780bb832af79c9339985d34933ef73e8
0dunola <jooodelola@gmail.com>
2022-06-18 11:08:01 +0000

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+/**
+ * reverse_array - reverse the content of an
+ * array of integers
+ * @a: given array
+ * @n: number of elements of the array
+ */
+void reverse_array(int *a, int n)
+{
+	int lt, rt, temp;
+
+	lt = 0;
+	rt = n - 1;
+
+	while (lt != rt)
+	{
+		temp = a[lt];
+		a[lt] = a[rt];
+		a[rt] = temp;
+		++lt, --rt;
+	}
+}
