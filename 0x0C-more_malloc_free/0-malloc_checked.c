ecdeb462785276386638e92d9f4c4c3d61d26aac
0dunola <jooodelola@gmail.com>
2022-07-01 14:32:45 +0000

    Task 1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memeory using malloc
+ * @b: amount of memory to allocate
+ * return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return ptr;
+}
+
