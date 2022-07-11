fb9bcb7686ac32138465214dc9f4cf1867e01bd6
0dunola <jooodelola@gmail.com>
2022-07-11 09:48:06 +0000

    Add task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+/**
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ * @array: given array
+ * @size: size of array
+ * @action: map function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		*(action)(array[i]);
+}
