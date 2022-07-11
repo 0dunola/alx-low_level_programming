f11ab088fc75f2a15b0943cb181c9ed14ea7421a
0dunola <jooodelola@gmail.com>
2022-07-11 09:56:03 +0000

    fix: add header for size_t

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+
 /**
  * array_iterator - executes a function given as a parameter
  * on each element of an array
@@ -7,8 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
+
+	if (!array || !action)
+		return;
 
 	for (i = 0; i < size; i++)
-		*(action)(array[i]);
+		action(array[i]);
 }
